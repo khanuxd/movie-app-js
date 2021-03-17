c1acfaaee497993ff6eebf441f6cd0220dcc004d
Anh <61621638+iamanh1990@users.noreply.github.com>
2021-03-17 14:47:13 +0200

    Delete footer.js

--- src/js/components/footer.js
+++
@@ -1,9 +0,0 @@
-export const footer = () => {
-  const footerHtmlString = `
-       <footer class="footer">
-        This is the footer. All Rights Reserved @ 2021.
-       </footer>
-    `;
-
-  return footerHtmlString;
-};
